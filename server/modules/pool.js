5d68fe7c0a8d78761b933bdb7a5bdaa1f8c1e2d6
unknown <mitchjolson@gmail.com>
2019-07-21 23:24:10 -0500

    changed DB pool settings to work with original DB

--- server/modules/pool.js
+++ server/modules/pool.js
@@ -33,8 +33,8 @@ if (process.env.DATABASE_URL) {
 
         //------------Set these back to null when connecting to DB on laptop------------------\\
 
-        user: process.env.PG_USER || 'postgres', //env var: PGUSER
-        password: process.env.DATABASE_SECRET || 'renovo4ever', //env var: PGPASSWORD
+        user: process.env.PG_USER || null, //env var: PGUSER
+        password: process.env.DATABASE_SECRET || null, //env var: PGPASSWORD
 
 
 
